b72d47e1ccd8f3899846f4c9a862122b3b4def98
kagi <k4gi@pm.me>
2022-05-25 16:07:19 +1000

    wee a game mechanic

--- main/GuildGame.gd
+++ main/GuildGame.gd
@@ -1,27 +1,49 @@
 extends Control
 
 
-onready var countlabel = $Margin/VBoxContainer/HBoxContainer/Label
-onready var countprogress = $Margin/VBoxContainer/HBoxContainer/ProgressBar
+onready var cutternum = $Margin/Grid/Woodcutters/VBox/HBoxWorkers/Num
+onready var woodnum = $Margin/Grid/Woodcutters/VBox/HBoxResource/Num
+onready var woodprogressbar = $Margin/Grid/Woodcutters/VBox/ProgressBar
 
-var count = 0
-var count_bar = 0
-var count_on = false
+onready var workersnum = $Margin/Grid/PanelContainer2/VBox/HBoxResource/Num
+onready var housenum = $Margin/Grid/PanelContainer2/VBox/HBoxResource2/Num
 
+var cuttermulti = 10
 
-func _process(_delta):
-	countlabel.set_text("%d" % count)
-	if count_on:
-		run_count_bar()
-		countprogress.set_value(count_bar)
-
-
-func _on_Button_toggled(button_pressed):
-	count_on = button_pressed
+var cutter = 1
+var wood = 2
+var woodprogress = 5
+var workers = 3
+var house = 4
 
 
-func run_count_bar():
-	count_bar += 1
-	if count_bar >= 100:
-		count += 1
-		count_bar = 0
+func _process(delta):
+	game_process(delta)
+	update_ui()
+
+
+func game_process(delta):
+	woodprogress += delta*cutter*cuttermulti
+	if woodprogress >= 100:
+		woodprogress = 0
+		wood += 1
+
+
+func update_ui():
+	cutternum.set_text(str(cutter))
+	woodnum.set_text(str(wood))
+	woodprogressbar.set_value(woodprogress)
+	workersnum.set_text(str(workers))
+	housenum.set_text(str(house))
+
+
+func _on_CutterMinus_pressed():
+	if cutter > 0:
+		cutter -= 1
+		workers += 1
+
+
+func _on_CutterPlus_pressed():
+	if workers > 0:
+		workers -= 1
+		cutter += 1
